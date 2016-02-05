e74c63585ed0153be3de87dcb34421f8d19ee977
Omak <UshurovaOmak>
2016-02-05 07:59:09 +0600

    created student class

---
+++ ConsoleApplication1/ConsoleApplication1/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Student
+    {
+        public string Name;
+        public string Surname;
+
+        public Student(string _name, string _surname)
+        {
+            this.Name = _name;
+            this.Surname = _surname;
+        }
+
+        public override string ToString()
+        {
+            return "This student name is " + Name + " " + Surname;
+        }
+    }
+}
